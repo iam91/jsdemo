7aa93f6dbcf4efa910f0185dc0f6581fac06f579
iam91 <zhengwy91@foxmail.com>
2016-04-22 23:03:00 +0800

    update

---
+++ tetris/tetris.js
@@ -0,0 +1,14 @@
+var field = document.getElementById('field');
+var block = document.createElement('div');
+block.setAttribute('class', 'block');
+block.setAttribute('style', 'left:30px;');
+var t = field.appendChild(block);
+var i = 0
+/*
+function fun(){
+	block.setAttribute('style', 'top: ' + i * 30 + 'px;');
+	i++;
+}
+setInterval(fun, 1000);
+*/
+
