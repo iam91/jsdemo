538213a6d7a7b531266c084f730fb22078635d07
iam91 <zhengwy91@foxmail.com>
2016-04-23 17:14:21 +0800

    add block stop and key strike event

--- tetris/run.js
+++ tetris/run.js
@@ -1,19 +1,24 @@
 var field = document.getElementById('field');
 
-var temp = new Tetris(T, [5, 5]);
+var freeSpace = new FreeSpace(25, 30);
+var temp = new Tetris(T, [12, 5], freeSpace);
+
+document.onkeydown = function(){
+	code = event.keyCode;
+	if(code == 32){
+		temp.spin(spinMetrix);
+	}
+	else if(code == 39){
+		temp.right();
+	}
+	else if(code == 37){
+		temp.left();
+	}
+	else if(code == 40){
+		temp.down();
+	}
+};
+
 temp.addToField(field);
-			alert('pause');
-temp.spin(spinMetrix);
-			alert('pause');
-temp.spin(spinMetrix);
-			alert('pause');
-temp.spin(spinMetrix);
-			alert('pause');
-temp.down();
-			alert('pause');
-temp.down();
-			alert('pause');
-temp.left();
-			alert('pause');
-temp.right();
+
 setInterval('temp.down()', 1000);
--- tetris/tetris.js
+++ tetris/tetris.js
@@ -1,22 +1,51 @@
 var BLOCK_SIZE = 20;
 var NUM_OF_BLOCKS_PER_TETRIS = 4;
 
-function Tetris(shape, centerPosition){
+function Tetris(shape, centerPosition, freeSpace){
 	this.shape = shape;
-	
 	this.blocks = Array(NUM_OF_BLOCKS_PER_TETRIS);
+	this.freeSpace = freeSpace;
 
 	this.centerPosition = centerPosition;
+	this.currPositions = Array(NUM_OF_BLOCKS_PER_TETRIS);
 
-	this.getBlocks = function(){
-		return this.blocks;
+	this.stop = false;
+
+	this.isStop = function(){
+		return this.stop;
+	};
+
+	this.computePositions = function(center, shape){
+		newPositions = Array(NUM_OF_BLOCKS_PER_TETRIS);
+		for(var i = 0; i < NUM_OF_BLOCKS_PER_TETRIS; i++){
+			newPositions[i] = [
+				center[0] + shape[i][0],
+				center[1] + shape[i][1] 
+			];
+		}
+		return newPositions;
+	};
+
+	this.canDo = function(newPositions){
+		flag = true;
+		for(var i = 0; i < NUM_OF_BLOCKS_PER_TETRIS; i++){
+			x = newPositions[i][0];
+			y = newPositions[i][1];
+			if(!this.freeSpace.isFree(x, y)){
+				flag = false;
+				break;
+			}
+		}
+		return flag;
 	};
 
 	this.physicalMovement = function(){
 		for(var i = 0; i < NUM_OF_BLOCKS_PER_TETRIS; i++){
 			this.blocks[i].setAttribute('style', 
-				'top:' + (BLOCK_SIZE * this.centerPosition[1] + BLOCK_SIZE * this.shape[i][1]) + 'px;' 
-				+ 'left:' + (BLOCK_SIZE * this.centerPosition[0] + BLOCK_SIZE * this.shape[i][0]) + 'px;');
+				'top:' + (BLOCK_SIZE * (this.centerPosition[1] + this.shape[i][1])) + 'px;' 
+				+ 'left:' + (BLOCK_SIZE * (this.centerPosition[0] + this.shape[i][0])) + 'px;');
+			this.currPositions[i][0] = this.centerPosition[0] + this.shape[i][0];
+			this.currPositions[i][1] = this.centerPosition[1] + this.shape[i][1]; 
 		}
 	};
 
@@ -25,34 +54,108 @@ function Tetris(shape, centerPosition){
 			this.blocks[i] = document.createElement('div');
 			this.blocks[i].setAttribute('class', 'block');
 			field.appendChild(this.blocks[i]);
+			this.currPositions[i] = [0, 0];
 		}
 		this.physicalMovement();
 	};
 	
 	this.spin = function(spinMetrix){
-		newShape = Array(4);
-		for(var i = 0; i < NUM_OF_BLOCKS_PER_TETRIS; i++){
-			newShape[i] = [
-				spinMetrix[0][0] * this.shape[i][0] + spinMetrix[1][0] * this.shape[i][1], 
-				spinMetrix[0][1] * this.shape[i][0] + spinMetrix[1][1] * this.shape[i][1]
-			];
+		if(!this.stop){
+			newShape = Array(NUM_OF_BLOCKS_PER_TETRIS);
+			for(var i = 0; i < NUM_OF_BLOCKS_PER_TETRIS; i++){
+				newShape[i] = [
+					spinMetrix[0][0] * this.shape[i][0] + spinMetrix[1][0] * this.shape[i][1], 
+					spinMetrix[0][1] * this.shape[i][0] + spinMetrix[1][1] * this.shape[i][1]
+				];
+			}
+			newPositions = this.computePositions(this.centerPosition, newShape);
+			if(this.canDo(newPositions)){
+				this.shape = newShape;
+				this.physicalMovement();
+			}
 		}
-		this.shape = newShape;
-		this.physicalMovement();
 	};
 
 	this.down = function(){
-		this.centerPosition[1]++;
-		this.physicalMovement();
+		if(!this.stop){
+			this.centerPosition[1]++;
+			newPositions = this.computePositions(this.centerPosition, this.shape);
+			if(this.canDo(newPositions)){
+				this.physicalMovement();
+			}
+			else{
+				this.centerPosition[1]--;
+				this.stop = true;
+				this.freeSpace.setOccupied(this.currPositions, this.blocks);
+			}
+		}
 	};
 
 	this.left = function(){
-		this.centerPosition[0]--;
-		this.physicalMovement();
+		if(!this.stop){
+			this.centerPosition[0]--;
+			newPositions = this.computePositions(this.centerPosition, this.shape);
+			if(this.canDo(newPositions)){
+				this.physicalMovement();
+			}
+			else{
+				this.centerPosition[0]++;
+			}
+		}
 	};
 
 	this.right = function(){
-		this.centerPosition[0]++;
-		this.physicalMovement();
+		if(!this.stop){
+			this.centerPosition[0]++;
+			newPositions = this.computePositions(this.centerPosition, this.shape);
+			if(this.canDo(newPositions)){
+				this.physicalMovement();
+			}
+			else{
+				this.centerPosition[0]--;
+			}
+		}
+	};
+}
+
+function FreeSpace(width, length){
+	this.width = width;
+	this.length = length;
+
+	this.freeSpace = (function(){
+		free = Array(width);
+		for(var i = 0; i < width + 2; i++){
+			free[i] = Array(length);
+			for(var j = 0; j < length + 1; j++){
+				if(i == 0 || i == width + 1 || j == length){
+					free[i][j] = [false, null];
+				}
+				else{
+					free[i][j] = [true, null];
+				}
+			}
+		}
+		return free;
+	})();
+
+	this.setOccupied = function(positions, blocks){
+		for(var i = 0; i < NUM_OF_BLOCKS_PER_TETRIS; i++){
+			x = positions[i][0];
+			y = positions[i][1];
+			this.freeSpace[x][y][0] = false;
+			this.freeSpace[x][y][1] = blocks[i];
+		}
+	};
+
+	this.isFree = function(x, y){
+		return this.freeSpace[x][y];
+	};
+
+	this.getWidth = function(){
+		return this.width;
+	};
+
+	this.getLength = function(){
+		return this.length;
 	};
 }
