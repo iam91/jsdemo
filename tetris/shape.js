7854ccb221af791fdbaba81c8a14b08579b2f8b4
iam91 <zhengwy91@foxmail.com>
2016-04-23 11:45:04 +0800

    add shape and shape spinning

--- tetris/shape.js
+++ tetris/shape.js
@@ -1,27 +1,32 @@
-var I = {
-	blocks: [[0, 0], [1, 0], [2, 0], [3, 0]];
-}
+var I = [[-1, 0], [0, 0], [1, 0], [2, 0]];
+var L = [[0, 0], [0, 1], [1, 0], [2, 0]];
+var J = [[-2, 0], [-1, 0], [0, 0], [0, 1]];
+var O = [[0, 0], [1, 0], [0, 1], [1, 1]];
+var Z = [[-1, 0], [0, 0], [0, 1], [1, 1]];
+var T = [[0, 0], [-1, 0], [1, 0], [0, -1]];
+var S = [[-1, 1], [0, 1], [0, 0], [1, 0]];
 
-var J = {
-	blocks: [[0, 0], [0, 1], [1, 0], [2, 0]];
-}
+var shapeList = [I, J, L, O, S, T, Z];
 
-var L = {
-	blocks: [[0, 0], [1, 0], [2, 0], [2, 1]];
-}
+var spinMetrix = [[0, 1], [-1, 0]];
 
-var O = {
-	blocks: [[0, 0], [1, 0], [0, 1], [1, 1]];
-}
-
-var S = {
-	blocks: [[0, 0], [1, 0], [1, 1], [2, 1]];
-}
-
-var T = {
-	blocks: [[0, 0], [1, 0], [2, 0], [1, 1]];
-}
-
-var Z = {
-	blocks: [[0, 0], [1, 0], [1, -1], [2, -1]];
+function spin(shape){
+	retspin = Array(4);
+	retspin[0] = [
+		spinMetrix[0][0] * shape[0][0] + spinMetrix[1][0] * shape[0][1], 
+		spinMetrix[0][1] * shape[0][0] + spinMetrix[1][1] * shape[0][1]
+		];
+	retspin[1] = [
+		spinMetrix[0][0] * shape[1][0] + spinMetrix[1][0] * shape[1][1], 
+		spinMetrix[0][1] * shape[1][0] + spinMetrix[1][1] * shape[1][1]
+		];
+	retspin[2] = [
+		spinMetrix[0][0] * shape[2][0] + spinMetrix[1][0] * shape[2][1], 
+		spinMetrix[0][1] * shape[2][0] + spinMetrix[1][1] * shape[2][1]
+		];
+	retspin[3] = [
+		spinMetrix[0][0] * shape[3][0] + spinMetrix[1][0] * shape[3][1], 
+		spinMetrix[0][1] * shape[3][0] + spinMetrix[1][1] * shape[3][1]
+		];
+	return retspin;
 }
--- tetris/tetris.js
+++ tetris/tetris.js
@@ -2,13 +2,34 @@ var field = document.getElementById('field');
 var block = document.createElement('div');
 block.setAttribute('class', 'block');
 block.setAttribute('style', 'left:30px;');
-var t = field.appendChild(block);
-var i = 0
-/*
+var i = 0;
+var shape = [
+	document.createElement('div'),
+	document.createElement('div'),
+	document.createElement('div'),
+	document.createElement('div')
+];
+addToField = function(field, T, b, shape){
+		shape[0].setAttribute('class', 'block');
+		shape[1].setAttribute('class', 'block');
+		shape[2].setAttribute('class', 'block');
+		shape[3].setAttribute('class', 'block');
+		shape[0].setAttribute('style', 'top:' + (100 + 20 * T[0][1]) + 'px;' + 'left:' + (100 + 20 * T[0][0]) + 'px;');
+		shape[1].setAttribute('style', 'top:' + (100 + 20 * T[1][1]) + 'px;' + 'left:' + (100 + 20 * T[1][0]) + 'px;');
+		shape[2].setAttribute('style', 'top:' + (100 + 20 * T[2][1]) + 'px;' + 'left:' + (100 + 20 * T[2][0]) + 'px;');
+		shape[3].setAttribute('style', 'top:' + (100 + 20 * T[3][1]) + 'px;' + 'left:' + (100 + 20 * T[3][0]) + 'px;');
+		//alert('buttom:' + (100 + 20 * T[3][1]) + 'px;' + 'left:' + (100 + 20 * T[3][0]) + 'px;');
+		field.appendChild(shape[0]);
+		field.appendChild(shape[1]);
+		field.appendChild(shape[2]);
+		field.appendChild(shape[3]);
+	};
+
+addToField(field, spin(shapeList[4]), block, shape);
+
 function fun(){
 	block.setAttribute('style', 'top: ' + i * 30 + 'px;');
 	i++;
 }
 setInterval(fun, 1000);
-*/
 
