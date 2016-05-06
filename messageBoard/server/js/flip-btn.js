e23a0055f22a90a3da8a9547858f4a33e3c31439
iam91 <zhengwy91@foxmail.com>
2016-05-06 16:59:57 +0800

    flip button style

--- messageBoard/server/js/flip-btn.js
+++ messageBoard/server/js/flip-btn.js
@@ -2,9 +2,16 @@ window.onload = function(){
 	var flipBtn = document.querySelector('.btn-flip');
 	var flipBtnFront = document.querySelector('.btn-flip .btn-flip-front');
 	var flipBtnBack = document.querySelector('.btn-flip .btn-flip-back');
+	var flipBtnYes = document.querySelector('.btn-flip-yes');
+	var flipBtnNo = document.querySelector('.btn-flip-no');
+	
 	flipBtnFront.onclick = function(){
-		flipBtn.classList.add('btn-flip-show');
 		flipBtnFront.classList.add('btn-flip-front-show');
 		flipBtnBack.classList.add('btn-flip-back-show');
 	};
+
+	flipBtnNo.onclick = function(){
+		flipBtnFront.classList.remove('btn-flip-front-show');
+		flipBtnBack.classList.remove('btn-flip-back-show');
+	}
 };
